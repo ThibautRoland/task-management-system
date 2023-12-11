3a82092758ddf91b44c4aed79d6256661d306219
thibaut roland <thibaut.roland2@gmail.com>
2023-12-11 17:13:41 +0100

    added columns to users table

---
+++ db/migrate/20231211152417_add_columns_to_users.rb
@@ -0,0 +1,11 @@
+class AddColumnsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :date_of_birth, :date
+    add_column :users, :title, :string
+    add_column :users, :clearance_level, :integer
+    add_column :users, :username, :string
+    add_column :users, :address, :string
+  end
+end
