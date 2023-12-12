1b47a3190c8b1a51df9dadc110f611b1f679b079
thibaut roland <thibaut.roland2@gmail.com>
2023-12-12 15:00:28 +0100

    added has many to models and sign in link in home

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+  has_many :tasks
+  has_many :project_members
+  has_many :users, through: :project_members
 end
--- app/models/project_member.rb
+++ app/models/project_member.rb
@@ -1,4 +1,5 @@
 class ProjectMember < ApplicationRecord
   belongs_to :project
   belongs_to :user
+  has_many :tasks
 end
